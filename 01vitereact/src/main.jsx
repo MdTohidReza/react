7749549ccb82266f75c83ca8214fdf0ead82da61
Md Tohid Reza <harishalone786@gmail.com>
2025-04-17 21:25:51 +0545

    creating react

--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -1,9 +1,19 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import React from 'react';
+import { createRoot } from 'react-dom/client';
 import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
- 
-    <App />
-  
+const anotherElement =(
+    <a href="https://google.com" target='_blank'> visit google</a>
 )
+createRoot(document.getElementById('root')).render(
+anotherElement
+)
+
+// const reactElement = React.createElement(
+//     'a',
+//     {href: 'https://google.com', target: '_blank'},
+//     'click me to visit google'
+// )
+// createRoot(document.getElementById('root')).render(
+// reactElement
+// )
