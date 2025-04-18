9f32c07f0e4777a4c2d40b2d2df48cfc4cabadbd
Md Tohid Reza <harishalone786@gmail.com>
2025-04-18 22:31:56 +0545

    second react counter task

---
+++ 02counter/src/App.jsx
@@ -0,0 +1,54 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  // counter is variable and setcounter is function which is responsible for updating the value of counter
+  // useState is a hook which is used to create state in functional component
+
+  let [counter, setCounter] = useState(0)
+  //let counter= 15
+// Note :- In React UI is controlled by React ownself
+
+  const addValue= ()=>{
+    if(counter === 20){
+      setCounter(20)
+      return
+    }
+    //counter = counter + 1
+    setCounter(counter + 1)
+    
+    // setcounter(counter)// if counter - counter+1 is defined
+  }
+
+
+
+  const removeValue= ()=>{
+    // console.log("Removed", counter);
+     if (counter === 0){
+      setCounter(0)
+      return
+    }
+    setCounter(counter - 1)
+  }
+
+  return (
+    <>
+    <h1>Chai aur React</h1>
+    <h2>Counter Value:{counter}</h2>
+
+    <button onClick={addValue}>Add value {counter}</button>
+    <br />
+    <br />
+    <button onClick={removeValue}>Remove Value{counter}</button>
+
+    <p>Footer:{counter}</p>
+    </>
+
+    
+  )
+}
+
+export default App
