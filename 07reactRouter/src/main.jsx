a2cb779b83a458597fc38761925869ae875b4fa4
Md Tohid Reza <harishalone786@gmail.com>
2025-05-05 22:41:35 +0545

    Router

--- 07reactRouter/src/main.jsx
+++ 07reactRouter/src/main.jsx
@@ -7,6 +7,8 @@ import Layout from './Layout.jsx'
 import Home from './components/Home/Home.jsx'
 import About from './components/About/About.jsx'
 import Contact from './components/contact/Contact.jsx'
+import User from './User/User.jsx'
+import Github, { githubInfoLoader } from './components/Github/Github.jsx'
 
 //Both are same
 
@@ -43,6 +45,12 @@ const router = createBrowserRouter(
   <Route path='' element={<Home/>}/>
   <Route path='about' element={<About/>}/>
   <Route path='contact' element={<Contact/>}/>
+  <Route path='user/:userid' element={<User/>}/>
+
+  <Route
+       loader={githubInfoLoader}
+       path='github'
+       element={<Github/>}/>
   </Route>
   )
 )
