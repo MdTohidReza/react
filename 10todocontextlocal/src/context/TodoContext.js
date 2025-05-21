6df6341b6bb0fff24aa1b13ee7a175b7f59bebac
Md Tohid Reza <harishalone786@gmail.com>
2025-05-21 22:09:42 +0545

    todocontext

---
+++ 10todocontextlocal/src/context/TodoContext.js
@@ -0,0 +1,22 @@
+import { createContext, useContext } from "react";
+
+export const TodoContext = createContext({
+    todos:[
+        {
+            id:1,
+            todo:"Todo Msg",
+            completed: true,
+        }
+    ],
+    addTodo:(todo) => {},
+    updateTodo:(id, todo) => {},
+    deleteTodo:(id)=>{},
+    toggleComleted:(id)=>{}
+
+})
+
+export const useTodo = ()=>{
+    return useContext(TodoContext)
+}
+
+export const TodoProvider = TodoContext.Provider
