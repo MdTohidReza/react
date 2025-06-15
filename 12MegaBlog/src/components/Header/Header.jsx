8e862842b8c0397046db679b924e7eca9e55faf7
Md Tohid Reza <harishalone786@gmail.com>
2025-06-15 22:37:45 +0545

    button and input field

--- 12MegaBlog/src/components/Header/Header.jsx
+++ 12MegaBlog/src/components/Header/Header.jsx
@@ -50,7 +50,13 @@ const Header = () => {
                 <button onClick={()=> navigate(item.slug)}
                   className='inline-block px-6 py-2 duration-200 hover:bg-blue-100 rounded-full'>{item.name}</button>
               </li>
-            ) : null)}
+            ) : null
+            )}
+            {authStatus && (
+            <li>
+              <LogoutBtn/>
+            </li>
+            )}
           </ul>
         </nav>
       </Container>
